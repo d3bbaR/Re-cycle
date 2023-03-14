5ea9ef1d6cb3cc4c8dd581f3c8f1e65ad9bdab2f
siemie16 <siemie16k@gmail.com>
2023-03-14 16:14:31 +0100

    verder

--- afspraken.php
+++ afspraken.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/agenda.css">
     <title>Afspraken</title>
 </head>
 <body>
     <?php
     $date = date("y-m-d");
+    echo $date;
     $dag = date("d");
-    $maand = date('F');
+    $naammaand = date('F');
     $jaar = date('y');
     $newDate = date('l', strtotime($date));
     $translate = array( 
@@ -35,11 +37,51 @@
         "December"=>"December",
 
     );
-  
+    $dagen = array(
+        "ma","di","wo","do","vr","za","zo"
+    );
+  echo $translate[$newDate]." ".$dag." ".$translate[$naammaand];
+  echo "<div class='tabel'>";
+  echo "<div class='naamdag'>";
+  foreach ($dagen as $day){
+    echo "<div class='naam'>".$day."</div>";
+  }
+  echo "</div>";
+  // opzoeken welke dag de eerste van de maand is
+    $maand = date('m');
+    $edag = $jaar."-".$maand."-"."01";
+    $emaand = $jaar."-".$maand-1."-"."01";
+    $naamedag = date('l',strtotime($edag));
 
-    echo $translate[$newDate]." ".$dag." ".$translate[$maand];
+    switch ($translate[$naamedag]) {
+        case 'Maandag':
+            $ervoor = 0;
+            break;
+        case 'Dinsdag':
+            $ervoor = 1;
+            break;
+        case 'Woensdag':
+            $ervoor = 2;
+            break;
+        case 'Donderdag':
+            $ervoor = 3;
+            break;
+        case 'Vrijdag':
+            $ervoor = 4;
+            break;
+        case 'Zaterdag':
+            $ervoor = 5;
+            break;
+        case 'Zondag':
+            $ervoor = 6;
+            break;
+    }
+    echo $translate[$naamedag]." ".$ervoor."<br>";
+    echo "<p>-".$ervoor." "."days</p>";
+    echo date($emaand, strtotime("-".strval($ervoor)." "."days"));
 
   
     ?>
+    </div>
 </body>
 </html>
