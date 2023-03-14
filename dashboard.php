8cfb9d71d5b58b9e4d3a71b296c897babc61b6a0
siemie16 <siemie16k@gmail.com>
2023-03-14 12:08:10 +0100

    dashboard v1

---
+++ dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+       $folder = scandir('PHP/R');
+       echo print_r($folder);
+       /*$translate = array( 
+        "account"=>"account beheer",
+        "allergenen"=>"allergenen beheer",
+        "bestellingen"=>"bestellingen beheer",
+        "categorie"=>"categorie beheer",
+        "foto"=>"foto beheer",
+        "gemeente"=>"gemeente beheer",
+        "gerecht"=>"gerecht beheer",
+        "gerecht_ingrediënt"=>"samenstelling beheer",
+        "ingrediënt"=> "ingrediënt beheer",
+       "ingrediënt_allergenen"=>"allergenen toewijzen",
+       "klanten"=>"klanten beheer",
+       "landcode"=>"landcode beheer",
+       "pw"=>"ignore",
+       "rol"=>"rol beheer",
+       "settings"=>"setting beheer"
+
+       ) */
+       
+    ;
+       
+       echo "</br></br></br><div class='driekol'>";
+       foreach ($folder as $file){
+        if(substr($file ,-4 ) == '.php'){
+            $filename = substr($file, 0, -4);
+            if ($translate[$filename] != "ignore"){
+            echo"<a href='PHP/R/$file'><button>".$filename."</button></a>";
+            }
+        }
+       }
+       echo "</div>"
+       ?>
+</body>
+</html>
--- login.php
+++ login.php
@@ -14,6 +14,61 @@ session_start();
 <body>
     <?php
     if(isset($_SESSION["naam"])){
+        if ($_SESSION["rol"] == 1){
+    ?>
+<header>
+        <section class="navigation">
+            <div class="nav-container">
+                <div class="brand">
+                    <img class="logonav" alt="Logo" src="assets/Re-cycle.png">
+                </div>
+                <nav>
+                    <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+                    <ul class="nav-list">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Afspraken</a>
+                        </li>
+                        <li>
+                            <a href="#">Nieuw</a>
+                        </li>
+                        <li>
+                            <a href="#">Cadeau ideeën</a>
+                        </li>
+                        <li>
+                            <a href="#">Fietsen</a>
+                        </li>
+                        <li>
+                            <a href="#">More</a>
+                        </li>
+                        <li>
+                            <a href="dashboard.php">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="login.php"><img class="taalpic" alt="Logo" src="assets/user-interface.png"></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+    </header>
+    <div class="container">
+        <div class="login">
+            <?php
+            echo "<h1>Welkom terug bij re-cycle!"." ".$_SESSION["naam"]."</h1>
+            <p>account naam:"." ".$_SESSION["naam"]." ".$_SESSION["familienaam"]." </p>
+            <p>account email:"." ".$_SESSION["email"]." </p>
+            <p>account telefoon:"." ".$_SESSION["telefoon"]." </p>";
+        ?>
+        <form method="post" action="logout.php">
+        <input type="submit" value="logout">
+        </form>
+        </div>
+    </div>
+    <?php }}
+    else {
     ?>
 <header>
         <section class="navigation">
@@ -50,60 +105,15 @@ session_start();
             </div>
         </section>
     </header>
-    <div class="container">
-        <div class="login">
-            <?php
-            echo "<h1>Welkom terug bij re-cycle!"." ".$_SESSION["naam"]."</h1>
-            <p>account naam:"." ".$_SESSION["naam"]." ".$_SESSION["familienaam"]." </p>
-            <p>account email:"." ".$_SESSION["email"]." </p>
-            <p>account telefoon:"." ".$_SESSION["telefoon"]." </p>";
-        ?>
-        <form method="post" action="logout.php">
-        <input type="submit" value="logout">
-        </form>
-        </div>
-    </div>
+    <?php
+    }
+    ?>
 <?php
-}
-else {
 ?>
 
 
 <header>
-        <section class="navigation">
-            <div class="nav-container">
-                <div class="brand">
-                    <img class="logonav" alt="Logo" src="assets/Re-cycle.png">
-                </div>
-                <nav>
-                    <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
-                    <ul class="nav-list">
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">Afspraken</a>
-                        </li>
-                        <li>
-                            <a href="#">Nieuw</a>
-                        </li>
-                        <li>
-                            <a href="#">Cadeau ideeën</a>
-                        </li>
-                        <li>
-                            <a href="#">Fietsen</a>
-                        </li>
-                        <li>
-                            <a href="#">More</a>
-                        </li>
-                        <li>
-                            <a href="login.php"><img class="taalpic" alt="Logo" src="assets/user-interface.png"></a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </section>
-    </header>
+        
     <div class="container">
         <div class="login">
             <h1>Welkom bij re-cycle!</h1>
@@ -130,7 +140,6 @@ else {
     </div>
 </body>
 <?php
-}
 ?>
 
 </html>
