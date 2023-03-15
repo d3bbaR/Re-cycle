ca35136227da4aae8feb7b3eebbdb0a686a84be6
siemie16 <siemie16k@gmail.com>
2023-03-15 12:46:35 +0100

    css files compacter

--- afspraken.php
+++ afspraken.php
@@ -6,19 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <link href="https://fonts.googleapis.com/css2?family=Signika:wght@300&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://use.fontawesome.com/1a4d35d4d9.js"></script>
     <title>Re-cycle</title>
     <link rel="shortcut icon" type="image/x-icon" href="assets/Logo ICON White.png">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"
-        type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet"
-        type="text/css" />
-    <link href="css/css.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/extra.css">
+    <link  rel="stylesheet"  href="css/css.css"/>
     <link rel="stylesheet" href="css/agenda.css">
 </head>
-
 <body>
     <?php
     session_start();
