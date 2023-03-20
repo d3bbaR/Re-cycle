bf7f42a2c095b15fbf350cbb6db4c3248e4ba9a4
siemie16 <siemie16k@gmail.com>
2023-03-20 08:53:01 +0100

    Update login.php

--- login.php
+++ login.php
@@ -141,6 +141,6 @@ session_start();
 </body>
 <?php
 ?>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
- <script src="js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="js/script.js"></script>
 </html>
