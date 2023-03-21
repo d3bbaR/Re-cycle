d491cec47bd0668fc4b1f0a504022a3543fd6990
briosi39cl <127833830+briosi39cl@users.noreply.github.com>
2023-03-21 11:33:45 +0100

    u

---
+++ header.php
@@ -0,0 +1,18 @@
+<div class="header2">
+  <div class="container2">
+      <div class="first">
+        <div class=box2>
+        <h2>Re-cycle</h2>
+   </div>
+   <a href="cat.php">Zie meer fietsen <i class="fa fa-angle-right"></a></i>
+   <div class="left">
+    <i  class="fa fa-facebook"></i>
+    </div>
+    <div class="second">
+        <div class="overlay">
+          
+        </div>
+    </div>
+</div>
+</div>
+</div>
