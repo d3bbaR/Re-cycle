f926920ee698af45cf5347383d3811543096060d
siemie16 <siemie16k@gmail.com>
2023-03-23 13:43:50 +0100

    fixed

--- afspraken.php
+++ afspraken.php
@@ -26,94 +26,6 @@
     include "PHP/functions.php";
     include "PHP/conn.php";
     include 'nav-bar.php';
-    session_start();
-    if(isset($_SESSION["naam"])){
-        if ($_SESSION["rol"] == 1){
-    ?>
-    <header>
-        <section class="navigation">
-            <div class="nav-container">
-                <div class="brand">
-                    <a href="index.php"><img class="logonav" alt="Logo" src="assets/Re-cycle.png"></a>
-                </div>
-                <nav>
-                    <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
-                    <ul class="nav-list">
-                        <li>
-                            <a href="index.php">Home</a>
-                        </li>
-                        <li>
-                            <a href="afspraken.php">Afspraken</a>
-                        </li>
-                        <li>
-                            <a href="#">Nieuw</a>
-                        </li>
-                        <li>
-                            <a href="#">Cadeau ideeën</a>
-                        </li>
-                        <li>
-                            <a href="#">Fietsen</a>
-                        </li>
-                        <li>
-                            <a href="#">More</a>
-                        </li>
-                        <li>
-                            <a href="dashboard.php">Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="login.php"><img class="taalpic" alt="Logo" src="assets/user-interface.png"></a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </section>
-    </header>
-            <?php
-        }
-    }
-        else {
-    
-            ?>
-
-    <header>
-        <section class="navigation">
-            <div class="nav-container">
-                <div class="brand">
-                    <a href="index.php"><img class="logonav" alt="Logo" src="assets/Re-cycle.png"></a>
-                </div>
-                <nav>
-                    <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
-                    <ul class="nav-list">
-                        <li>
-                            <a href="index.php">Home</a>
-                        </li>
-                        <li>
-                            <a href="afspraken.php">Afspraken</a>
-                        </li>
-                        <li>
-                            <a href="#">Nieuw</a>
-                        </li>
-                        <li>
-                            <a href="#">Cadeau ideeën</a>
-                        </li>
-                        <li>
-                            <a href="#">Fietsen</a>
-                        </li>
-                        <li>
-                            <a href="#">More</a>
-                        </li>
-                        <li>
-                            <a href="login.php"><img class="taalpic" alt="Logo" src="assets/user-interface.png"></a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </section>
-    </header>
-    <?php
-        }
-    ?>
-    <?php
     $date = date("Y-m-d");
     $dag = date("d");
     $naammaand = date('F');
@@ -771,6 +683,6 @@
     <?php
     }
     ?>
-    <script src="js/agendar.js"></script>
+    <script src="js/agenda2.js"></script>
 </body>
 </html> 
