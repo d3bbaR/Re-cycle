750e2f9a86cfad037735d879ba5898578223597a
siemie16 <siemie16k@gmail.com>
2023-03-27 14:32:29 +0200

    nextupd

--- PHP/R/afspraken.php
+++ PHP/R/afspraken.php
@@ -5,20 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/css.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../../css/agendar.css">
     <title>afsrpaken </title>
 </head>
 <body>
-    <?php include '../../afspraken.php';?>  
+    <?php include '../../afsprakenfuncties.php';?>  
 
     <div class="afsprakenbevestigen">
 
         <?php
         foreach (query($selector) as $res) {
-            echo "hallo ";
-            # code...
+            if ($res["gekeurd"] == 1){
+
+            }
+            else{
+            echo "<div class ='tb'>".$res["naam"]." ".$res["email"]." ".$res["telefoon"]." ".$res["type"]." ".$res["dagen"]." ".$res["uren"].
+            "<form action='../C/accept.php' method='post'> <button name ='edit' value='".$res["FK_geg"]."'><i class='fa fa-check' style='color:green'></i></button></form>".
+            "<form action='../C/refuse.php' method='post'> <button name ='delete' value='".$res["FK_geg"]."'<i class='fa fa-close' style='color:red'></i></button></form></div>";
+            }
         }
 
         ?>
+        <script src="../../js/agenda2.js"></script>
 </body>
 </html>
