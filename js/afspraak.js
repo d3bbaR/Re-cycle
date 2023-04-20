a12c5c0f918868c12c4ec86053cd6a1e2be1343f
siemie16 <siemie16k@gmail.com>
2023-04-20 13:52:40 +0200

    Update afspraak.js

--- js/afspraak.js
+++ js/afspraak.js
@@ -1,24 +1,25 @@
 let x = document.getElementById("hoeveel");
 x = x.innerHTML;
 let tel = 0;
-document.onload = console.log("hellooooooo");
+document.onload = bezet(); 
+function bezet(){
 while (tel < x) {
    
     tel =+1;
-    console.log(tel);
     let modal = document.getElementById("d"+tel);
     let uur = document.getElementById("uur"+tel);
     modal = modal.innerHTML;
+    console.log(modal);
     uur = uur.innerHTML;
     let afspr = document.getElementById(modal);
+    console.log(afspr);
     dag = afspr.innerHTML;
-    console.log(dag);
     let modalplaats = document.getElementById("d"+uur+"hhhh"+dag);
     modalplaats.setAttribute("class", "gesl afspr");
     afspr.setAttribute("class" , "dag afspr");   
-    console.log(modal);
-    
-    
+   
+
+}
 }
 function modalappear() {
     console.log("reee");
