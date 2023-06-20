37f7d76f961eb3cf4053f10592cc47831a56264f
siemie16 <siemie16k@gmail.com>
2023-06-20 20:29:32 +0200

    upd

--- PHP/R/catalogus.php
+++ PHP/R/catalogus.php
@@ -10,24 +10,24 @@
 </head>
 
 <body>
-    <?php include '../../nav-bar2.php'; ?>
-    <form action="../upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="FuResume" id="FuResume">
-        <button type="submit">upload file</button>
-
-
-    </form>
-    <?php
-    if (isset($_GET["succes"])) {
-        echo "<p style='color:green'>Je hebt de file succesvol geupload.</p>";
-    }
-    if (isset($_GET["goed"])) {
-        echo "<p style='color:green'>Je hebt de catalogus succesvol bijgewerkt.</p>";
-    }
-    ?>
-    <form action="../readfile.php" method="post">
-        <button type="submit"> vernieuw de catalogus</button>
-    </form>
+    <div class="uploadcontainer">
+        <?php include '../../nav-bar2.php'; ?>
+        <form class="uploadform" action="../upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="FuResume" id="FuResume">
+            <button type="submit">upload file</button>
+        </form>
+        <?php
+        if (isset($_GET["succes"])) {
+            echo "<p style='color:green'>Je hebt de file succesvol geupload.</p>";
+        }
+        if (isset($_GET["goed"])) {
+            echo "<p style='color:green'>Je hebt de catalogus succesvol bijgewerkt.</p>";
+        }
+        ?>
+        <form action="../readfile.php" method="post">
+            <button type="submit"> vernieuw de catalogus</button>
+        </form>
+    </div>
 </body>
 
 </html>
