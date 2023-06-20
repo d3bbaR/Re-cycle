c2a9a6cb9ad8117f729bf2de3e52e322ef76c5e2
siemie16 <siemie16k@gmail.com>
2023-06-20 19:08:24 +0200

    upd

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if (isset($_GET["mail"])) {
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
-
+    <script src="js/script.js"></script>
 </body>
 
 </html>
